tokio-rs/tracing

chore: remove *all* `extern crate` statements (#1738)
Depends on #1737

This branch removes all remaining `extern crate` statements. Most of
these are in old code and were not removed when updating to Rust 2018.
Whoops!

Our MSRV no longer requires `extern crate`, so we don't need these. The
exception is `extern crate` statements for `std` and `alloc`, which are
still the way these libraries are included explicitly when building for
`no_std` platforms.

In some cases, the tests had to explicitly import the `span!` and
`event!` macros at every use, because their names conflict with the
`span` and `event` modules in the test support code. Oh well.

Signed-off-by: Eliza Weisman <eliza@buoyant.io>

tracing/test_static_max_level_features/tests/test.rs
@@ -1,9 +1,6 @@
-#[macro_use]
-extern crate tracing;
-
 use std::sync::{Arc, Mutex};
 use tracing::span::{Attributes, Record};
-use tracing::{span, Collect, Event, Id, Level, Metadata};
+use tracing::{debug, error, info, span, trace, warn, Collect, Event, Id, Level, Metadata};
 use tracing_core::span::Current;
 
 struct State {

openharmony/third_party_rust_tracing

tracing/test_static_max_level_features/tests/test.rs
@@ -1,9 +1,6 @@
-#[macro_use]
-extern crate tracing;
-
 use std::sync::{Arc, Mutex};
 use tracing::span::{Attributes, Record};
-use tracing::{span, Event, Id, Level, Metadata, Subscriber};
+use tracing::{debug, error, info, span, trace, warn, Event, Id, Level, Metadata, Subscriber};
 
 struct State {
     last_level: Mutex<Option<Level>>,